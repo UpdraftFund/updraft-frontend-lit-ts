68292e135387acd27051e32651d822f93771a55e
Amirerfan <amirerfan.siamaki@gmail.com>
2024-11-22 17:49:11 +0100

    feat: add navbar component

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { Router } from '@lit-labs/router';
 
@@ -6,11 +6,24 @@ import { theme } from './styles/theme';
 
 import './components/index.ts';
 import './pages/components-preview';
+import './components/layout/navbar';
 
 @customElement('my-app')
 export class MyApp extends LitElement {
   static styles = [
-    theme
+    theme,
+    css`
+      :host {
+        display: block;
+        padding-top: 64px;
+      }
+
+      main {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 2rem;
+      }
+    `
   ];
   
   private router = new Router(this, [
@@ -21,14 +34,19 @@ export class MyApp extends LitElement {
     {
       path: '/',
       render: () => html`
-        <h1>Home Page</h1>
-        <app-button label="Click me"></app-button>
+        <main>
+          <h1>Home Page</h1>
+          <app-button label="Click me"></app-button>
+        </main>
       `
     }
   ]);
 
   render() {
-    return html`${this.router.outlet()}`;
+    return html`
+      <app-navbar></app-navbar>
+      ${this.router.outlet()}
+    `;
   }
 }
 
