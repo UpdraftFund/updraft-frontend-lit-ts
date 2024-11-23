b591e90c48c9e003e40828944428a04a5ec56dc5
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-22 16:48:54 -0800

    fix network type; export wagmi config

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,28 @@
-import { createAppKit } from '@reown/appkit'
-import { arbitrumSepolia } from '@reown/appkit/networks'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { createAppKit } from '@reown/appkit';
+import { AppKitNetwork, arbitrumSepolia, arbitrum } from '@reown/appkit/networks';
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 
-const projectId = 'a259923fc99520ecad30021b33486037'
+const projectId = 'a259923fc99520ecad30021b33486037';
 
-export const networks = [arbitrumSepolia]
+export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [arbitrumSepolia, arbitrum];
 
 const wagmiAdapter = new WagmiAdapter({
   projectId,
-  networks
-})
+  networks,
+});
 
 const metadata = {
   name: 'Updraft',
   description: 'Get paid to crowdfund and work on public goods.',
   url: 'https://updraft.fund', // origin must match your domain & subdomain
-  icons: ['https://avatars.githubusercontent.com/u/187091561']
-}
+  icons: ['https://avatars.githubusercontent.com/u/187091561'],
+};
 
-// 3. Create the modal
-const modal = createAppKit({
+createAppKit({
   adapters: [wagmiAdapter],
   networks,
   metadata,
   projectId,
-  features: {
-    analytics: true // Optional - defaults to your Cloud configuration
-  }
-})
+});
+
+export const config = wagmiAdapter.wagmiConfig;
