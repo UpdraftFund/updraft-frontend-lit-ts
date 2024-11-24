c78ae18572d95d1dcad0b1c5b13010fd98a9f8a5
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-24 02:33:02 -0800

    add more contract functions including write functions

--- src/contracts/updraft.ts
+++ src/contracts/updraft.ts
@@ -1,4 +1,5 @@
-import { readContract } from '@wagmi/core';
+import { readContract, simulateContract, writeContract, getTransactionReceipt } from '@wagmi/core';
+import { trim } from 'viem';
 import { config } from '../web3.ts';
 import addresses from './updraftAddresses.json';
 import abi from './abis/Updraft.json';
@@ -9,21 +10,69 @@ type AddressMap = {
 
 const updraftAddresses: AddressMap = addresses as AddressMap;
 
-const updraftAddress = () => {
+export const updraftAddress = () => {
   const address: `0x${string}` = updraftAddresses[config.getClient().chain.name];
   return address;
 };
 
-const feeToken = async () => {
+//region Read Functions
+export const percentScale = async () => {
+  return await readContract(config, {
+    abi,
+    address: updraftAddress(),
+    functionName: 'percentScale',
+  });
+};
+
+export const feeToken = async () => {
   return await readContract(config, {
     abi,
     address: updraftAddress(),
     functionName: 'feeToken',
   });
+};
+
+export const minFee = async () => {
+  return await readContract(config, {
+    abi,
+    address: updraftAddress(),
+    functionName: 'minFee',
+  });
 }
 
-export {
-  updraftAddress,
-  feeToken,
+export const percentFee = async () => {
+  return await readContract(config, {
+    abi,
+    address: updraftAddress(),
+    functionName: 'percentFee',
+  });
+}
+//endregion
+
+//region Write Functions
+export const updateProfile = async (profileData) => {
+  const { request } = await simulateContract(config, {
+    abi,
+    address: updraftAddress(),
+    functionName: 'updateProfile',
+    args: [profileData],
+  });
+  await writeContract(config, request);
 }
 
+export const createIdea = async (contributorFee, contribution, ideaData):`0x${string}` => {
+  const { request } = await simulateContract(config, {
+    abi,
+    address: updraftAddress(),
+    functionName: 'createIdea',
+    args: [contributorFee, contribution, ideaData],
+  });
+  const hash = await writeContract(config, request);
+  const receipt = await getTransactionReceipt(config, { hash });
+//   console.log('Transaction receipt');
+//   console.dir(receipt);
+
+  // address of the new Idea contract
+  return trim(receipt.logs[0].topics[1]);
+}
+//endregion
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import { toHex, parseUnits } from 'viem';
 import { createConnection } from './web3.ts';
-import { updraftAddress, feeToken } from './contracts/updraft.ts';
+import { updraftAddress, feeToken, updateProfile, createIdea, percentScale } from './contracts/updraft.ts';
 
 createConnection();
 
@@ -7,7 +8,7 @@ const b1 = document.getElementById("updraft-address-button");
 const e1 = document.getElementById("updraft-address");
 if(b1 && e1){
   b1.addEventListener("click", () => {
-    e1.innerHTML = `Updraft address: ${updraftAddress()}`;
+    e1.textContent = `Updraft address: ${updraftAddress()}`;
   });
 }
 
@@ -16,7 +17,33 @@ const e2 = document.getElementById("feeToken");
 if (b2 && e2){
   b2.addEventListener("click", async () => {
     const feeTokenAddress = await feeToken();
-    e2.innerHTML = `Fee token address: ${feeTokenAddress}`;
+    e2.textContent = `Fee token address: ${feeTokenAddress}`;
   });
 }
 
+const profileButton = document.getElementById("updateProfile-button");
+const profile = document.getElementById("profile");
+if (profileButton && profile){
+  profileButton.addEventListener("click", async () => {
+    updateProfile(toHex(profile.value));
+  });
+}
+
+const form = document.getElementById('idea-form')
+if (form) {
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const scale = await percentScale();
+    const contributorFee = document.getElementById('contributor-fee');
+    const contribution = document.getElementById('contribution');
+    const ideaData = document.getElementById('idea-data');
+    const ideaAddress = document.getElementById('idea-address');
+    if(contributorFee && contribution && ideaData && ideaAddress){
+      ideaAddress.textContent = await createIdea(
+        BigInt(contributorFee.value) * scale / 100n,
+        parseUnits(contribution.value, 18),
+        toHex(ideaData.value)
+      );
+    }
+  });
+}
