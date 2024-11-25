855f5296de7b2748b9e2263e9b5ba4df54745596
adamstallard <205792+adamstallard@users.noreply.github.com>
2024-11-25 01:47:21 -0800

    more methods for Idea class

--- src/contracts/idea.ts
+++ src/contracts/idea.ts
@@ -12,17 +12,34 @@ export class Idea {
   constructor(public address: `0x${string}`) {}
 
   //region Read methods
-  async checkPosition(owner: `0x${string}`, positionIndex: bigint): Promise<ideaPosition> {
+  async checkPosition(funder: `0x${string}`, positionIndex?: bigint): Promise<ideaPosition> {
     const output = await readContract(config, {
       abi,
       address: this.address,
       functionName: 'checkPosition',
-      args: [owner, positionIndex],
+      args: positionIndex ? [funder, positionIndex] : [funder],
     }) as [bigint, bigint];
     return {
       tokens: output[0],
       shares: output[1],
     };
+  }
+
+  async contributorFee(): Promise<bigint> {
+    return await readContract(config, {
+      abi,
+      address: this.address,
+      functionName: 'contributorFee',
+    }) as bigint;
+  }
+
+  async numPositions(funder: `0x${string}`): Promise<bigint> {
+    return await readContract(config, {
+      abi,
+      address: this.address,
+      functionName: 'numPositions',
+      args: [funder],
+    }) as bigint;
   }
   //endregion
 
@@ -33,6 +50,16 @@ export class Idea {
       address: this.address,
       functionName: 'contribute',
       args: [amount],
+    });
+    await writeContract(config, request);
+  }
+
+  async withdraw(positionIndex?: bigint) {
+    const { request } = await simulateContract(config, {
+      abi,
+      address: this.address,
+      functionName: 'withdraw',
+      args: positionIndex ? [positionIndex] : undefined,
     });
     await writeContract(config, request);
   }
