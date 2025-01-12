ec93f970f903828826b9db3b636ce802f6d99c35
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-01-12 00:43:29 -0800

    plus icon button

--- src/components/layout/profile-area.ts
+++ src/components/layout/profile-area.ts
@@ -4,12 +4,18 @@ import { customElement, state } from 'lit/decorators.js';
 import { modal } from '../../web3';
 
 import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
+import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
 
 @customElement('profile-area')
 export class ProfileArea extends LitElement {
   static styles = css`
     img {
       border-radius: 50%;
+    }
+    sl-icon-button {
+      font-size: 2rem;
+      color: var(--main-foreground);
     }
   `
   @state() connected = false;
@@ -24,7 +30,24 @@ export class ProfileArea extends LitElement {
   }
 
   render() {
-    return html`
+    return this.connected ?
+    html`
+      <sl-icon-button src="/assets/icons/plus-lg.svg"></sl-icon-button>
+      <sl-dropdown>
+        <span slot="trigger">
+          <img src="/path/to/avatar.jpg" alt="User avatar" />
+          <span>${this.address}</span>
+        </span>
+
+        <sl-menu>
+          <sl-menu-item value="profile">Profile</sl-menu-item>
+          <sl-menu-item value="settings">Settings</sl-menu-item>
+          <sl-divider></sl-divider>
+          <sl-menu-item value="logout">Logout</sl-menu-item>
+        </sl-menu>
+       </sl-dropdown>
+    `
+    : html`
       <sl-button pill variant="primary" @click=${() => modal.open()} >Connect Wallet</sl-button>
     `;
   }
