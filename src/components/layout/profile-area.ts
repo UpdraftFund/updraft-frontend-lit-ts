b1ccc4c7b77acbf57a0aa712cb9147358281ef2d
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-01-12 23:37:04 -0800

    start of profile menu

--- src/components/layout/profile-area.ts
+++ src/components/layout/profile-area.ts
@@ -5,6 +5,9 @@ import { consume } from '@lit/context';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
+import '@shoelace-style/shoelace/dist/components/menu/menu.js';
+import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 
 import { modal } from '../../web3';
 import { User, userContext } from '../../user-context';
@@ -17,6 +20,10 @@ export class ProfileArea extends LitElement {
       display: flex;
       align-items: center;
       gap: 12px;
+      color: var(--main-foreground);
+    }
+    .trigger-content {
+      cursor: pointer;
     }
     .name {
       display: inline-block; /* Ensures the span respects the width */
@@ -41,17 +48,24 @@ export class ProfileArea extends LitElement {
     return this.user.connected ?
     html`
       <sl-icon-button src="/assets/icons/plus-lg.svg"></sl-icon-button>
-      <sl-dropdown>
+      <sl-dropdown distance="20" placement="top-end">
         <span slot="trigger" class="trigger-content">
           <img src="${this.user.avatar}" alt="User avatar" />
           <span class="name">${this.user.name || this.user.address}</span>
         </span>
-
         <sl-menu>
-          <sl-menu-item value="profile">Profile</sl-menu-item>
-          <sl-menu-item value="settings">Settings</sl-menu-item>
-          <sl-divider></sl-divider>
-          <sl-menu-item value="logout">Logout</sl-menu-item>
+          <sl-menu-item>
+            <span>
+              <sl-icon src="/assets/icons/lightbulb.svg"></sl-icon>
+              Choose Network
+            </span>
+            <p class="status">Mainnet</p>
+          </sl-menu-item>
+          <sl-menu-item>Buy Gas Tokens</sl-menu-item>
+          <sl-menu-item>Swap for UPD</sl-menu-item>
+          <sl-menu-item>View Profile</sl-menu-item>
+          <sl-menu-item>Activity</sl-menu-item>
+          <sl-menu-item>Disconnect</sl-menu-item>
         </sl-menu>
        </sl-dropdown>
     `
