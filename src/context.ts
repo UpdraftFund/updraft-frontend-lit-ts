9a2cad112b61001e61ac7cc0bc22831f028cf357
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-01-16 01:21:15 -0800

    Make event type readonly

--- src/context.ts
+++ src/context.ts
@@ -27,7 +27,7 @@ export const userContext = createContext<User>('updraft-user');
 export const balanceContext = createContext<Balances>('balances');
 
 export class RequestBalanceRefresh extends Event {
-  static type = 'request-balance-refresh';
+  static readonly type = 'request-balance-refresh';
   constructor() {
     super(RequestBalanceRefresh.type, { bubbles: true, composed: true });
   }
