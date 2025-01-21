9c3cc7a24e56691c7b3182a37b732c3d1ef5e9f3
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-01-20 23:38:37 -0800

    .entries() not needed

--- src/components/base/saveable-form.ts
+++ src/components/base/saveable-form.ts
@@ -15,7 +15,7 @@ export class SaveableForm extends LitElement {
       const formData = new FormData(this.form);
       const formObject: Record<string, string> = {};
 
-      for (const [key, value] of formData.entries()) {
+      for (const [key, value] of formData) {
         if (typeof value === "string") {
           formObject[key] = value; // Save the string value (even if it's empty)
         } else {
