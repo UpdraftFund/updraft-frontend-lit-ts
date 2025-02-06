69c3429ab9aa5b5ff0703c8eab8deffea6e84ad8
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-02-06 13:09:56 -0800

    dialog for getting UPD and checking balance

--- src/components/upd-dialog.ts
+++ src/components/upd-dialog.ts
@@ -1,7 +1,15 @@
 import { customElement, query } from "lit/decorators.js";
 import { css, html, LitElement } from "lit";
+import { consume } from "@lit/context";
+
+import calculator from "../assets/icons/calculator.svg"
+
+import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/dialog/dialog.js";
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 import { SlDialog } from "@shoelace-style/shoelace";
+
+import { balanceContext, Balances, RequestBalanceRefresh } from "../context.ts";
 
 @customElement('upd-dialog')
 export class UpdDialog extends LitElement {
@@ -11,9 +19,35 @@ export class UpdDialog extends LitElement {
       color: var(--main-foreground);
       background-color: var(--main-background);
     }
+    
+    sl-dialog::part(title) {
+      font-weight: bold;
+      font-size: 1.5rem;
+    }
+    
+    .label{
+      margin: 0;
+    }
+    
+    sl-dialog::part(body) {
+      padding-top: 0;
+    }
+
+    .check-balance {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+    }
   `
 
   @query('sl-dialog') dialog!: SlDialog;
+
+  @consume({ context: balanceContext, subscribe: true }) balances!: Balances;
+
+
+  private handleRefreshBalance() {
+    this.dispatchEvent(new RequestBalanceRefresh());
+  }
 
   async show() {
     this.dialog.show();
@@ -21,7 +55,20 @@ export class UpdDialog extends LitElement {
 
   render() {
     return html`
-      <sl-dialog label="Get more UPD">
+      <sl-dialog>
+        <p slot="label" class="label">Get more UPD</p>
+        <span class="check-balance">
+          <p>You have <span class="balance">${this.balances?.updraft?.balance || '0'}</span> UPD</p>
+          <sl-button pill @click=${this.handleRefreshBalance} variant="primary" size="small">
+            <sl-icon slot="prefix" class="calculator-icon" src=${calculator}/></sl-icon>
+            Recheck Balance
+          </sl-button>
+        </span>
+        <h3>How to get UPD</h3>
+        <ul>
+          <li><a href="https://discord.gg/mQJ58MY6Nz" target="_blank" rel="noopener">
+            Get free UPD from our discord
+          </a></li>
       </sl-dialog>
     `;
   }
