969a47db1607f48ba844e4aa2237b48588736a03
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-02-09 00:24:59 -0800

    improve links on view-profile.ts

--- src/pages/view-profile.ts
+++ src/pages/view-profile.ts
@@ -12,9 +12,9 @@ import '../components/layout/page-heading.ts'
 import '../components/layout/left-side-bar.ts'
 import '../components/layout/activity-feed.ts'
 
-import urqlClient from '../urql-client';
 import { User, userContext } from '../context';
 
+import urqlClient from '../urql-client';
 import { ProfileDocument } from '../../.graphclient';
 
 @customElement('view-profile')
@@ -39,23 +39,14 @@ export class ViewProfile extends LitElement {
       box-sizing: border-box;
       display: flex;
       flex-direction: column;
-    }
-
-    .profile-content {
-      display: flex;
-      flex-direction: column;
-      gap: 1.2rem;
-      margin: 1rem 3rem;
+      padding: 1rem 2rem;
       color: var(--main-foreground);
     }
 
     .avatar {
-      background: var(--main-background);
       border-radius: 50%;
       width: 64px;
       height: 64px;
-      padding: 0.2rem;
-      display: flex;
     }
 
     .avatar img {
@@ -84,9 +75,6 @@ export class ViewProfile extends LitElement {
       font-weight: 300;
       color: var(--subtle-text);
       font-size: 0.8rem;
-      margin-top: 0.2rem;
-      margin-bottom: 0.5rem;
-      text-align: center;
     }
 
     .profile-info h3 {
@@ -107,14 +95,25 @@ export class ViewProfile extends LitElement {
       font-size: 1.2rem;
       margin-bottom: 0.5rem;
     }
-
-    .links-section a {
-      color: var(--sl-color-primary-600);
-      text-decoration: none;
+    
+    .links-section {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
     }
 
-    .links-section a:hover {
+    .link {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      text-decoration: none;
+      color: var(--main-foreground);
+      width: fit-content;
+    }
+
+    .link:hover {
       text-decoration: underline;
+      color: var(--sl-color-primary-600);
     }
 
     @media (max-width: 1415px) {
@@ -136,8 +135,7 @@ export class ViewProfile extends LitElement {
   address!: string;
 
   @consume({ context: userContext })
-  @state()
-  user?: User;
+  @state() user?: User;
 
   private readonly profile = new Task(this, {
     task: async () => {
@@ -145,7 +143,7 @@ export class ViewProfile extends LitElement {
         let result;
         try {
           result = await urqlClient.query(ProfileDocument, { userId: this.address });
-          if(result.data?.user?.profile) {
+          if (result.data?.user?.profile) {
             return JSON.parse(fromHex(result.data.user.profile as `0x${string}`, 'string'));
           }
         } catch (error) {
@@ -157,6 +155,37 @@ export class ViewProfile extends LitElement {
     args: () => [this.address] as const
   });
 
+  private createLink(link: string) {
+    let url;
+    try {
+      url = new URL(link);
+    } catch {
+      try {
+        url = new URL(`https://${link}`);
+      } catch {
+        return;
+      }
+    }
+    const pathname = url.pathname.replace(/^\/+/, ''); // strip leading slashes
+    return html`
+        <a class="link" href="${url.href}" target="_blank">
+          <img
+              src=${`https://www.google.com/s2/favicons?domain=${link}&sz=16`}
+              @error=${(e: Event) => this.handleImageError(e)}
+              alt="Logo for ${link}"
+              width="16px"
+              height="16px"
+          />
+          <span>${pathname || url.host}</span>
+        </a>
+    `;
+  }
+
+  private handleImageError(event: Event) {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = '/src/assets/icons/link-45deg.svg'; // Fallback icon
+  }
+
   render() {
     return html`
       <top-bar></top-bar>
@@ -165,45 +194,41 @@ export class ViewProfile extends LitElement {
         <main>
           ${this.profile.render({
             complete: (value) => html`
-              <div class="profile-content">
-                <div class="profile-header">
-                  <div class="avatar">
-                    <img src="${value?.image || makeBlockie(this.address)}" alt="Profile avatar">
-                  </div>
-                  <div>
-                    ${value?.name ? html`
-                      <h3>${value.name}</h3>
-                    ` : ''}
-                    <span class="address">${this.address}</span>
-                    ${value?.team ? html`
-                      <p class="team">${value.team}</p>
-                    ` : ''}
-                  </div>
+              <div class="profile-header">
+                <div class="avatar">
+                  <img src="${value?.image || makeBlockie(this.address)}" alt="Profile avatar">
                 </div>
-
-                ${value?.about ? html`
-                  <div class="about-section">
-                    <h4 class="section-heading">About</h4>
-                    <p>${value.about}</p>
-                  </div>
-                ` : ''}
-
-                ${value?.news ? html`
-                  <div class="news-section">
-                    <h4 class="section-heading">Latest Updates</h4>
-                    <p>${value.news}</p>
-                  </div>
-                ` : ''}
-
-                ${value?.links?.length ? html`
-                  <div class="links-section">
-                    <h4 class="section-heading">Links</h4>
-                    ${value.links.map((link: string) => html`
-                      <p><a href="${link}" target="_blank" rel="noopener noreferrer">${link}</a></p>
-                    `)}
-                  </div>
-                ` : ''}
+                <div>
+                  ${value?.name ? html`
+                    <h3>${value.name}</h3>
+                  ` : ''}
+                  <span class="address">${this.address}</span>
+                  ${value?.team ? html`
+                    <p class="team">${value.team}</p>
+                  ` : ''}
+                </div>
               </div>
+
+              ${value?.about ? html`
+                <div class="about-section">
+                  <h4 class="section-heading">About</h4>
+                  <p>${value.about}</p>
+                </div>
+              ` : ''}
+
+              ${value?.news ? html`
+                <div class="news-section">
+                  <h4 class="section-heading">Latest Updates</h4>
+                  <p>${value.news}</p>
+                </div>
+              ` : ''}
+
+              ${value?.links?.length ? html`
+                <div class="links-section">
+                  <h4 class="section-heading">Links</h4>
+                  ${value.links.map((link: string) => this.createLink(link))}
+                </div>
+              ` : ''}
             `
           })}
         </main>
