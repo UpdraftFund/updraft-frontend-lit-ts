815e7fcb608b354152aa94a45f9712bf8bc13c4f
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-02-14 12:27:37 -0800

    Add Discover page with route and basic layout
    Introduce a new '/discover' route, dynamically loading the Discover page component. Implement the basic structure for the Discover page with a sidebar, top bar, and a styled main section. This enhances app navigation and prepares for future Discover page features.

---
+++ src/pages/discover-page.ts
@@ -0,0 +1,47 @@
+import { customElement } from 'lit/decorators.js';
+import { css, html, LitElement } from 'lit';
+
+import '@layout/top-bar'
+import '@layout/left-side-bar'
+
+@customElement('home-page')
+export class HomePage extends LitElement {
+
+  static styles = css`
+    .container {
+      display: flex;
+      flex: 1 1 auto;
+      overflow: hidden;
+    }
+
+    left-side-bar {
+      flex: 0 0 274px;
+    }
+
+    main {
+      flex: 1;
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+      gap: .2rem;
+      padding: .5rem 1rem;
+      color: var(--main-foreground);
+      background: var(--main-background);
+    }
+  `;
+
+  render() {
+    return html`
+      <top-bar></top-bar>
+      <div class="container">
+        <left-side-bar></left-side-bar>
+        <main></main>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'home-page': HomePage;
+  }
+}
