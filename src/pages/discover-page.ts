f00d0813b7cca22e55e567f78ee9b884f7b6075c
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-02-16 23:13:52 -0800

    start of discover-page.ts queries, but doesn't work yet.

--- src/pages/discover-page.ts
+++ src/pages/discover-page.ts
@@ -1,18 +1,34 @@
 import { customElement, property } from 'lit/decorators.js';
 import { css, html, LitElement } from 'lit';
+import { consume } from "@lit/context";
+import { Task } from '@lit/task';
 
+import '@shoelace-style/shoelace/dist/components/tab-group/tab-group.js';
+import '@shoelace-style/shoelace/dist/components/tab/tab.js';
 import '@layout/top-bar';
 import '@layout/left-side-bar';
 import '@components/search-bar';
 
-import '@shoelace-style/shoelace/dist/components/tab-group/tab-group.js';
-import '@shoelace-style/shoelace/dist/components/tab/tab.js';
+import { connectionContext } from '@/context.ts';
+import { Connection, Idea, Solution } from "@/types";
+
+import urqlClient from "@/urql-client.ts";
+import {
+  IdeasBySharesDocument,
+  IdeasByFundersDocument,
+  IdeasByStartTimeDocument,
+  SolutionsByDeadlineDocument,
+  IdeasFullTextDocument,
+  IdeasByTagsDocument,
+} from "@gql";
+
+type QueryType = 'hot-ideas' | 'new-ideas' | 'deadline' | 'followed' | 'search' | 'tags';
 
 @customElement('discover-page')
 export class DiscoverPage extends LitElement {
 
   static styles = css`
-    
+
     .search-tabs {
       display: flex;
       align-items: center;
@@ -42,7 +58,68 @@ export class DiscoverPage extends LitElement {
     }
   `;
 
-  @property() tab: string | null = null;
+  private readonly queries = {
+    'hot-ideas': IdeasBySharesDocument,
+    'new-ideas': IdeasByStartTimeDocument,
+    'deadline': SolutionsByDeadlineDocument,
+    'followed': IdeasByFundersDocument,
+    'search': IdeasFullTextDocument,
+    'tags': IdeasByTagsDocument,
+  } as const;
+
+  private readonly variables = {
+    'hot-ideas': () => ({}),
+    'new-ideas': () => ({}),
+    'deadline': () => ({}),
+    'followed': () => ({
+      funders: JSON.parse(localStorage.getItem('funders') || '[]')
+    }),
+    'search': () => ({
+      text: this.search
+    }),
+    'tags': () => {
+      const tagMatches = this.search?.match(/\[.*?\]/g) || [];
+      const tags = tagMatches.map(tag => tag.replace(/[\[\]]/g, ''))
+        .slice(0, 5); // only get up to 5 matches
+      const defaultTag = tags[0] || '';
+      return {
+        tag1: tags[0] || defaultTag,
+        tag2: tags[1] || defaultTag,
+        tag3: tags[2] || defaultTag,
+        tag4: tags[3] || defaultTag,
+        tag5: tags[4] || defaultTag
+      };
+    }
+  };
+
+  private readonly resultEntities = {
+    'hot-ideas': 'ideas',
+    'new-ideas': 'ideas',
+    'deadline': 'solutions',
+    'followed': 'ideaContributions',
+    'search': 'ideaSearch',
+    'tags': 'ideas',
+  }
+
+  private readonly results = new Task(this, {
+    task: async ([tab]): Promise<any | void> => {
+      let queryType = tab;
+      if (queryType === 'search' && this.search?.startsWith('[')){
+        queryType = 'tags';
+      }
+      const query = queryType && this.queries[queryType];
+      if (query) {
+        const variables = this.variables[queryType!]?.() || {};
+        const result = await urqlClient.query(query, variables);
+        return result.data;
+      }
+    },
+    args: () => [this.tab, this.search] as const
+  });
+
+  @consume({ context: connectionContext, subscribe: true }) connection!: Connection;
+
+  @property() tab: QueryType | null = null;
   @property() search: string | null = null;
 
   private handleTab(e: any) {
@@ -65,7 +142,18 @@ export class DiscoverPage extends LitElement {
       </top-bar>
       <div class="container">
         <left-side-bar location="discover"></left-side-bar>
-        <main></main>
+        <main>
+          ${this.results.render({
+            complete: (data) => {
+              const result = data?.[this.resultEntities[this.tab!]] || [];
+              return html`
+                ${result.map((idea: Idea | Solution) => html`
+                  <idea-card .idea=${idea}></idea-card>
+                `)}
+              `
+            }
+          })}
+        </main>
     `;
   }
 }
