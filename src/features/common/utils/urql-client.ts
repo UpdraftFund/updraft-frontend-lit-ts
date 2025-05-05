48ded947812ba4ffd54bba3942824204d3120b7a
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-05-05 15:10:32 -0700

    use latest published subgraph query url

--- src/features/common/utils/urql-client.ts
+++ src/features/common/utils/urql-client.ts
@@ -3,7 +3,7 @@ import { Client, cacheExchange, fetchExchange } from '@urql/core';
 //TODO: each chain subgraph will need its own urql client
 
 const urqlClient = new Client({
-  url: 'https://api.studio.thegraph.com/query/94944/updraft/version/latest',
+  url: 'https://gateway.thegraph.com/api/subgraphs/id/AX96zuXixk4ugPQG7CDypy1EdRnYE4Z9khjk2fpzHfAq',
   exchanges: [cacheExchange, fetchExchange],
   requestPolicy: 'cache-and-network',
 });
