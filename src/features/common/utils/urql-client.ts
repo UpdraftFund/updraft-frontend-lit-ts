a04c1fa92b5ff2a1628eab3daa00c98c966ca7a3
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-05-05 19:52:57 -0700

    prodUrl (arb-sepolia) subgraph

--- src/features/common/utils/urql-client.ts
+++ src/features/common/utils/urql-client.ts
@@ -2,8 +2,21 @@ import { Client, cacheExchange, fetchExchange } from '@urql/core';
 
 //TODO: each chain subgraph will need its own urql client
 
+const prod = false;
+
+const devUrl = 'https://api.studio.thegraph.com/query/94944/updraft/1.19.1';
+const prodUrl =
+  'https://gateway.thegraph.com/api/subgraphs/id/AX96zuXixk4ugPQG7CDypy1EdRnYE4Z9khjk2fpzHfAq';
+const url = prod ? prodUrl : devUrl;
+const apiKey = '';
+
 const urqlClient = new Client({
-  url: 'https://api.studio.thegraph.com/query/94944/updraft/version/latest',
+  url,
+  fetchOptions: {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  },
   exchanges: [cacheExchange, fetchExchange],
   requestPolicy: 'cache-and-network',
 });
