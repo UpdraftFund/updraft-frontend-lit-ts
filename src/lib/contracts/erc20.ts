99c0ac4c4fd4872a49cf74f62c769a36f78ca5b7
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-05-08 21:33:52 -0700

    erc 20 contract

---
+++ src/lib/contracts/erc20.ts
@@ -0,0 +1,19 @@
+import { Abi } from 'abitype';
+
+import abi from './abis/ERC20.json';
+import { Contract } from './contract';
+
+/**
+ * ERC20 Contract Interface
+ *
+ * This class provides a standard interface for interacting with ERC20 tokens
+ * using the OpenZeppelin ERC20 standard interface.
+ *
+ * @see https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
+ */
+
+export class ERC20 extends Contract {
+  constructor(address: `0x${string}`) {
+    super(abi as Abi, address);
+  }
+}
