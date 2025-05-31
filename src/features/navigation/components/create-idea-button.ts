521cbdaba0a49de07228a1ddfed4eff66107a754
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-05-31 03:42:45 -0700

    hide top bar create-idea-button.ts on discover page

--- src/features/navigation/components/create-idea-button.ts
+++ src/features/navigation/components/create-idea-button.ts
@@ -3,13 +3,16 @@ import { customElement } from 'lit/decorators.js';
 
 import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
 import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
+
 import plusLgIcon from '@icons/navigation/plus-lg.svg';
+
+import { nav } from '@state/navigation';
 
 @customElement('create-idea-button')
 export class CreateIdeaButton extends LitElement {
   static styles = css`
-    :host {
-      display: inline-block;
+    .hidden {
+      display: none;
     }
     sl-icon-button::part(base) {
       color: var(--main-foreground);
@@ -25,7 +28,10 @@ export class CreateIdeaButton extends LitElement {
 
   render() {
     return html`
-      <sl-tooltip content="create an idea">
+      <sl-tooltip
+        class="${nav.get() === 'discover' ? 'hidden' : ''}"
+        content="create an idea"
+      >
         <sl-icon-button
           src="${plusLgIcon}"
           href="/create-idea"
