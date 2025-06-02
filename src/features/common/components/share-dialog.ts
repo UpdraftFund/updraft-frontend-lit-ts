b8833f0bf4f4cc3a78b259b22defd0f963c2dd68
adamstallard <205792+adamstallard@users.noreply.github.com>
2025-06-01 18:32:35 -0700

    remove comment

--- src/features/common/components/share-dialog.ts
+++ src/features/common/components/share-dialog.ts
@@ -60,7 +60,6 @@ export class ShareDialog extends LitElement {
 
   private async copyLink() {
     try {
-      // Include the share message along with the URL
       await navigator.clipboard.writeText(`${this.shareMessage} ${this.url}`);
       this.clipboardTip.content = 'Copied!';
     } catch {
